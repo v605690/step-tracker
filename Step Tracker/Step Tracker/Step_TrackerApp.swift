62315236f7f5f80726119ba2700f613f9fefa9f8
v605690 <v605690@gmail.com>
2024-05-19 14:55:57 -0500

    Created xcode project
    Step Tracker

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Crus Techno on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
